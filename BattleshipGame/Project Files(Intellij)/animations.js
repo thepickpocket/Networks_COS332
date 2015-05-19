8b50f94c2d6fb2427c020715b538d0ae2bd10c12
Jason Richard Evans <evans.richard.jason@gmail.com>
2015-05-19 18:50:06 +0200

    Animations added on all files.

--- BattleshipGame/Project Files(Intellij)/animations.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $('#Text').show("slow", "swing");
-})
--- BattleshipGame/Project Files(Intellij)/src/GridContent.java
+++ BattleshipGame/Project Files(Intellij)/src/GridContent.java
@@ -352,13 +352,14 @@ public class GridContent {
     }
 
     private String getWinner(){
-        return "<div class=\"container-fluid\" id=\"Text\">" +
+        return "<div class=\"container-fluid\" id=\"Text\" hidden>" +
                 "   <div class=\"row\">" +
                 "       <div class=\"col-md-4\"></div>" +
                 "       <div class=\"col-md-4 text-center\" style=\"background-color: rgba(0,0,0, 0.7); margin-top: 12%;\">" +
                 "           <h1 style=\"color: white; text-shadow: 3px 1px 3px #09b6ff;\">Congratulations</h1>" +
                 "           <p style=\"color: white;\">You have overthrown the enemy and the sea is now yours. Go forth and attack more ships and make even more of the ocean yours. Keep on going like this and you will be noted in history as the best battleship commander.</p>" +
-                "           <i class=\"fa fa-ship fa-5x\" style=\"color:white;\"></i>" +
+                "           <i class=\"fa fa-ship fa-5x\" style=\"color:white;\"></i><br /><br />" +
+                "           <a href=\"index.html\"><button class='btn btn-default'>New Game</button></a>" +
                 "       </div>" +
                 "       <div class=\"col-md-4\"></div>" +
                 "</div>";
